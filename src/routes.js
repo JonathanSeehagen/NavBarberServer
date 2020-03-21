228284c8fcd8753b84b6d48321b16e9d5ab77fa6
Jonathan <jonathanseehagen@gmail.com>
2020-03-21 16:19:30 -0300

    Session and Auth implementation

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-// User
+// User - Usuários
 routes.get('/users', UserController.index);
 routes.post('/users', /* validateUserStore, */ UserController.store);
 routes.put('/users/:id', /* validateUserStore, */ UserController.update);
+// ===
+
+// Sessions - Login
+routes.post(
+  '/sessions',
+  /* bruteForce.prevent, validateSessionStore, */ SessionController.store
+);
 // ===
 
 export default routes;
