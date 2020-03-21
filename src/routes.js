7c09053602688d655c8f5b4151e8160f1f2ce516
Jonathan <jonathanseehagen@gmail.com>
2020-03-21 16:43:16 -0300

    Implementation of Session and User Validation

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,24 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
+import validateSessionStore from './app/validators/SessionStore';
+
 const routes = new Router();
 
 // User - Usuários
 routes.get('/users', UserController.index);
-routes.post('/users', /* validateUserStore, */ UserController.store);
-routes.put('/users/:id', /* validateUserStore, */ UserController.update);
+routes.post('/users', validateUserStore, UserController.store);
+routes.put('/users/:id', validateUserUpdate, UserController.update);
 // ===
 
 // Sessions - Login
 routes.post(
   '/sessions',
-  /* bruteForce.prevent, validateSessionStore, */ SessionController.store
+  /* bruteForce.prevent, */
+  validateSessionStore,
+  SessionController.store
 );
 // ===
 
